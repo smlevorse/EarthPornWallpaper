85a61d75591dd93d54fa2a9eb7d8efe5909a50b6
smlevorse <smlevorse@msn.com>
2014-12-02 20:29:44 -0500

    Now sleeps for 30 minutes between changing backgrounds

--- EarthPornWallpaper/EarthPornWallpaper/Program.cs
+++ EarthPornWallpaper/EarthPornWallpaper/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Net;
 using System.IO;
+using System.Threading;
 
 namespace EarthPornWallpaper
 {
@@ -14,13 +15,17 @@ namespace EarthPornWallpaper
         public const string appPath = "C:\\Users\\Sean\\git\\EarthPornWallpaper\\EarthPornWallpaper\\EarthPornWallpaper";
         static void Main(string[] args)
         {
+            //constants
+            const int minutes = 30;
+            string subreddit = "http://www.reddit.com/r/earthporn/top/?sort=top&t=day";
+
             WebClient client = new WebClient();
             string htmlLoc = appPath + "earthporn.html";
             string earthpornHtml;
             string entry;
             string path;
             string title;
-            List<string> paths = new List<string>();
+            Queue<string> paths = new Queue<string>();
             int entryLoc;
             int imageEnd;
             int imageLoc;
@@ -30,83 +35,143 @@ namespace EarthPornWallpaper
             bool success;
             int titleLoc;
             int titleEnd;
-
-            //Download the most recent Earthporn top website
-            try
+            string aspectRatio;
+            string height;
+            string width;
+            double ratio;
+            int aspectStart;
+            int aspectEnd;
+            int x;              //the location of the X in the aspect ratio
+            bool foundRatio;
+            double h, w;
+            
+            while (true)
             {
-                client.DownloadFile("http://www.reddit.com/r/earthporn/top/?sort=top&t=day", htmlLoc);
-            }
-            catch (WebException e)
-            {
-                Console.WriteLine("An error occured when downloading r/earthporn\n" + e.Message);
-            }
-
-            //in a loop, download the 5 top posts right now
-            while (picCount < 5 && postCount < 20)
-            {
-                //read the html file
-                earthpornHtml = File.ReadAllText(htmlLoc);
-
-                /* look for the top post
-                 * it should be in a div that contains a span:
-                 * <span class="rank">
-                 */
-                entryLoc = earthpornHtml.IndexOf("<span class=\"rank\">" + postCount);
-                /* look for the start of the a tag that contains
-                 * the link to the image:
-                 * <a class="thumbnail may-blank " href="IMAGEURL
-                 */
-                entryLoc = earthpornHtml.IndexOf("<a class=\"thumbnail may-blank \"", entryLoc);
-                imageEnd = earthpornHtml.IndexOf(">", entryLoc) - 2;
-                entry = earthpornHtml.Substring(entryLoc, imageEnd - entryLoc);
-
-                //break the url out of the entry
-                imageLoc = entry.IndexOf("href") + 6;
-                URL = entry.Substring(imageLoc);
-
-                Console.WriteLine("Post {0} URL: " + URL, postCount);
-
-                /* Get the title of the post, it should be wrapped in a 
-                 * <a> tag, but as the content instead of an attribute
-                 * <a class="title may-blank href="IMAGEURL" tabindex="1">
-                 *      ::before
-                 *      "POSTTITLE"
-                 * </a>
-                 */
-
-                titleLoc = earthpornHtml.IndexOf("<a class=", imageEnd);
-                titleEnd = earthpornHtml.IndexOf("</a>", titleLoc);
-                title = earthpornHtml.Substring(titleLoc, titleEnd - titleLoc);
-                titleLoc = title.IndexOf(">") + 1;
-                title = title.Substring(titleLoc);
-                Console.WriteLine("Post {0} Title: " + title, postCount);
-
-                //try to download
+                //Download the most recent Earthporn top website
                 try
                 {
-                    path = DownloadImage(URL, postCount);
-                    paths.Add(path);
-                    success = true;
+                    client.DownloadFile(subreddit, htmlLoc);
                 }
                 catch (WebException e)
                 {
-                    Console.WriteLine("An error occured when downloading the image\n" + e.Message);
-                    success = false;
+                    Console.WriteLine("An error occured when downloading r/earthporn\n" + e.Message);
                 }
-                catch (FormatException f)
+
+                //in a loop, download the 5 top posts right now
+                while (picCount < 12 && postCount < 24)
                 {
-                    Console.WriteLine("The file found was not an image");
-                    success = false;
+                    //read the html file
+                    earthpornHtml = File.ReadAllText(htmlLoc);
+
+                    /* look for the top post
+                     * it should be in a div that contains a span:
+                     * <span class="rank">
+                     */
+                    entryLoc = earthpornHtml.IndexOf("<span class=\"rank\">" + postCount);
+                    /* look for the start of the a tag that contains
+                     * the link to the image:
+                     * <a class="thumbnail may-blank " href="IMAGEURL
+                     */
+                    entryLoc = earthpornHtml.IndexOf("<a class=\"thumbnail may-blank \"", entryLoc);
+                    imageEnd = earthpornHtml.IndexOf(">", entryLoc) - 2;
+                    entry = earthpornHtml.Substring(entryLoc, imageEnd - entryLoc);
+
+                    //break the url out of the entry
+                    imageLoc = entry.IndexOf("href") + 6;
+                    URL = entry.Substring(imageLoc);
+
+                    Console.WriteLine("Post {0} URL: " + URL, postCount);
+
+                    /* Get the title of the post, it should be wrapped in a 
+                     * <a> tag, but as the content instead of an attribute
+                     * <a class="title may-blank href="IMAGEURL" tabindex="1">
+                     *      ::before
+                     *      "POSTTITLE"
+                     * </a>
+                     */
+
+                    titleLoc = earthpornHtml.IndexOf("<a class=", imageEnd);
+                    titleEnd = earthpornHtml.IndexOf("</a>", titleLoc);
+                    title = earthpornHtml.Substring(titleLoc, titleEnd - titleLoc);
+                    titleLoc = title.IndexOf(">") + 1;
+                    title = title.Substring(titleLoc);
+                    Console.WriteLine("Post {0} Title: " + title, postCount);
+
+                    //Check for aspect ratio between 1.25 and 1.85
+                    //1.25 is the earliest TV screens, 1.85 is the cinema standard
+                    x = 0;
+                    aspectStart = 0;
+                    foundRatio = false;
+                    aspectRatio = "";
+                    ratio = 1.5;        //this will set the default within the range, so if we can't determine the ratio, it won't throw out a perfectly useful image
+                    for (int i = 0; i < title.Length - 1; i++)
+                    {
+                        if (!foundRatio)
+                        {
+                            aspectStart = title.IndexOf('[', i);
+                            if (aspectStart >= 0)
+                            {
+                                aspectEnd = title.IndexOf(']', aspectStart);
+                                if (aspectEnd >= 0)
+                                {
+                                    aspectRatio = title.Substring(aspectStart + 1, aspectEnd - aspectStart - 1).ToLower();
+                                    x = aspectRatio.IndexOf("x");
+                                    if (x >= 0)
+                                        foundRatio = true;
+                                }
+                            }
+                        }
+                    }
+
+                    if (foundRatio)
+                    {
+                        aspectStart = 0;
+                        width = aspectRatio.Substring(0, x);
+                        height = aspectRatio.Substring(x + 1);
+                        if (double.TryParse(height, out  h) && double.TryParse(width, out w))
+                        {
+                            ratio = w / h;
+                        }
+                    }
+
+                    if (ratio >= 1.25 && ratio <= 1.85)
+                    {
+                        //try to download
+                        try
+                        {
+                            path = DownloadImage(URL, postCount);
+                            paths.Enqueue(path);
+                            Console.WriteLine("Downloaded post " + postCount);
+                            success = true;
+                        }
+                        catch (WebException e)
+                        {
+                            Console.WriteLine("An error occured when downloading the image\n" + e.Message);
+                            success = false;
+                        }
+                        catch (FormatException f)
+                        {
+                            Console.WriteLine("The file found was not an image");
+                            success = false;
+                        }
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                    postCount++;
+                    if (success)
+                        picCount++;
                 }
 
-                postCount++;
-                if (success)
-                    picCount++;
+                while (paths.Count > 0)
+                {
+                    //set the walpaper if there is an appropriate path
+                    if (!paths.Peek().Equals(""))
+                        SetWallpaper(paths.Dequeue());
+                    Thread.Sleep(60000 * minutes);
+                }
             }
-
-            //set the walpaper if there is an appropriate path
-            if(!paths[1].Equals(""))
-                SetWallpaper(paths[1]);
 
         }
 
